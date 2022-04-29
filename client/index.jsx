b4302db1c0c2d0b37652c8ac193f534cacac9196
odinfs <72218018+odinfs@users.noreply.github.com>
2022-04-30 00:28:40 +0200

    integrate react with parcel in express

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import ReactDom from "react-dom";
 
-ReactDom.render(<h1>Hello films</h1>, document.getElementById("app"));
+ReactDom.render(
+  <h1>Hello films and stuff </h1>,
+  document.getElementById("app")
+);
